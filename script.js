ba41ae2d7d71f0a016bcce2c1ad41c0b9755cd02
Mohammad Yousuf <mohammadbil1998@gmail.com>
2024-03-09 21:22:17 -0600

    Ability to Update the Gameboard

--- script.js
+++ script.js
@@ -15,37 +15,45 @@ function createPlayer(name, marker) {
 
   const isPlayersTurn = () => playerTurn;
 
-  const startPlayersTurn = function() {
-    if(playerTurn == false) 
-    return playerTurn = true;
-  }
+  const startPlayersTurn = function () {
+    if (playerTurn == false) return (playerTurn = true);
+  };
 
-  const endPlayersTurn = function() {
-    if(playerTurn == true) 
-    return playerTurn == false;
-  }
+  const endPlayersTurn = function () {
+    if (playerTurn == true) return playerTurn == false;
+  };
 
   return { name, marker, isPlayersTurn, endPlayersTurn, startPlayersTurn };
+}
+
+function updateGameboard(row, col, player, gameboard) {
+  gameboard[row][col] = player.marker;
+  return gameboard;
 }
 
 function playGame() {
   const playerOne = createPlayer("Player-1", "X");
   const playerTwo = createPlayer("Player-2", "O");
+  const gameboardObj = gameboard;
 
-  console.log(gameboard);
+  console.log(updateGameboard(2, 0, playerOne, gameboardObj));
+  console.log(updateGameboard(2, 2, playerTwo, gameboardObj));
+  console.log(updateGameboard(0, 0, playerOne, gameboardObj));
+  console.log(updateGameboard(1, 1, playerTwo, gameboardObj));
+  console.log(updateGameboard(1, 0, playerOne, gameboardObj));
 
-  console.log(playerOne.isPlayersTurn());
-  console.log(playerTwo.isPlayersTurn());
-
-  console.log(playerOne.startPlayersTurn());
-  console.log(playerTwo.startPlayersTurn());
-
-  console.log(playerOne.endPlayersTurn());
-
-  return { playerOne, playerTwo }
+  return { playerOne, playerTwo };
 }
 
-
-playGame()
+playGame();
 
 // Testing
+// console.log(gameboard);
+
+// console.log(playerOne.isPlayersTurn());
+// console.log(playerTwo.isPlayersTurn());
+
+// console.log(playerOne.startPlayersTurn());
+// console.log(playerTwo.startPlayersTurn());
+
+// console.log(playerOne.endPlayersTurn());
