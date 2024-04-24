6a9822639f7872461cec6ebff8bb496ef9e02d9c
Mohammad Yousuf <mohammadbil1998@gmail.com>
2024-04-23 23:29:35 -0500

    Adding Methods to use with Game Object

--- script.js
+++ script.js
@@ -13,26 +13,51 @@ const gameboard = (function() {
 })();
 
 function createPlayer(name, marker) {
-    return {name, marker};
+    let playersTurn = false;
+
+    const startPlayersTurn = () => playersTurn = true;
+    const endPlayersTurn = () => playersTurn = false;
+
+    return {name, marker, startPlayersTurn, endPlayersTurn};
 }
 
 function playGame() {
-    // Swtiching Players
+    const playerOne = createPlayer("Player-1", "X");
+    const playerTwo = createPlayer("Player-2", "O");
 
+    // Swtiching Players
+    function changePlayersTurn() {
+
+    }
 
     // Winning Condition
-
+    function checkWinner() {
+        console.log("Winner");
+    }
 
     // Draw Condition
-
+    function checkIfDraw() {
+        console.log("Draw");
+    }
 
     // Player Moves
+    function makeMove() {
+        console.log("Move Made");
+    }
 
+    // Cell Validation
+    function checkIfCellEmpty() {
+        console.log("Cell Empty");
+    }
 
     // Game Over
+    function checkIfGameOver() {
+        console.log("Game Over");
+    }
 
+    return {changePlayersTurn, checkWinner, checkIfDraw, makeMove, checkIfGameOver};
 }
 
 // Some Testing
 console.log(gameboard)
-console.log(createPlayer("Player1", "X"))
+const game = playGame();
